fc53de2d2a56df272e50c8bc512971380b19809e
george <weseelight@139.com>
2021-05-30 13:57:38 +0800

    删去js里无用的

--- js/strings.js
+++
@@ -1,7 +0,0 @@
-loadTimeData.data = {
-    "fontfamily": "system-ui, sans-serif",
-    "fontsize": "75%",
-    "language": "en",
-    "logoKey": "HelloFrom47.640052,-122.142311",
-    "textdirection": "ltr"
-};
